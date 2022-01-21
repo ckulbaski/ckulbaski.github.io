24e21bfeba25eb1d8ab1e764c8c86fc5bc07c38f
Conner Kulbaski <connerkulbaski@gmail.com>
2022-01-21 11:51:41 -0600

    added icons

--- index.js
+++ index.js
@@ -13,15 +13,11 @@ function adjustText(){
         document.getElementById('name').style.fontSize = "2em"
         document.getElementById('email-text').style.display = "none"
         document.getElementById('git-text').style.display = "none"
-        document.getElementById('email-icon').class = "fas fa-envelope fa-lg"
-        document.getElementById('git-icon').class = "fab fa-github fa-lg"
     }
     else if(height < width){
         console.log("height < width")
         document.getElementById('name').style.fontSize = "4em"
         document.getElementById('email-text').style.display = "inline"
         document.getElementById('git-text').style.display = "inline"
-        document.getElementById('email-icon').class = "fas fa-envelope"
-        document.getElementById('git-icon').class = "fab fa-github"
     } 
 }
